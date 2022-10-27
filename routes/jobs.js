628c24ff3153759bbbe75b6ee19ca21a2d565e62
Renato Macedo <renato_macedo_1@hotmail.com>
2022-10-27 14:21:47 +0100

    popoulated db and implemented filters backend

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 const {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
-} = require('../controllers/jobs')
+} = require("../controllers/jobs");
 
-router.route('/').post(createJob).get(getAllJobs)
+router.route("/").post(createJob).get(getAllJobs);
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob);
 
-module.exports = router
+module.exports = router;
